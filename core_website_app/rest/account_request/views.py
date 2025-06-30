e6a9ddbaff0f8ab8e1ca3c3dbe67bffd02641ef2
Guillaume-sousa <guillaume.sousa@nist.gov>
2025-06-30 09:46:29 -0400

    feat: openapi schema doc
    Change-Id: I17a5d0b056ca778b9d96cf91a48bc37c840eda34

--- core_website_app/rest/account_request/views.py
+++ core_website_app/rest/account_request/views.py
@@ -5,6 +5,12 @@ import logging
 
 from django.http import Http404
 from django.utils.decorators import method_decorator
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    OpenApiResponse,
+    extend_schema,
+    OpenApiParameter,
+)
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -24,32 +30,38 @@ from core_website_app.rest.account_request.serializers import (
 logger = logging.getLogger("core_website_app.rest.account_request.views")
 
 
+@extend_schema(
+    tags=["Account Request"],
+    description="Create or get all Account Request",
+)
 class AccountRequestList(APIView):
     """Create or get all Account Request"""
 
+    @extend_schema(
+        summary="Get all account requests",
+        description="Get all account requests",
+        responses={
+            200: AccountRequestSerializer(many=True),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     @method_decorator(api_staff_member_required())
     def get(self, request):
         """Get all account requests
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 200
               content: List of account requests
-            - code: 400
-              content: Validation error
+            - code: 500
+              content: Internal server error
         """
         try:
             account_request_list = account_request_api.get_all()
-
             # Serialize object
             serializer = AccountRequestSerializer(
                 account_request_list, many=True
             )
-
             # Return response
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as api_exception:
@@ -58,26 +70,32 @@ class AccountRequestList(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Create a new account request",
+        description="Create a new account request",
+        request=UserSerializer,
+        responses={
+            201: AccountRequestSerializer,
+            400: OpenApiResponse(
+                description="Validation error / missing parameters"
+            ),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     def post(self, request):
         """Create a new account request
-
         Parameters:
-
             {
-                "username": "username",
-                "first_name": "first_name",
-                "last_name": "last_name",
-                "password": "password",
-                "email": "email"
+              "username": "username",
+              "first_name": "first_name",
+              "last_name": "last_name",
+              "password": "password",
+              "email": "email"
             }
-
         Args:
-
             request: HTTP request
-
         Returns:
-
-            - code: 200
+            - code: 201
               content: Account Request
             - code: 400
               content: Validation error / missing parameters
@@ -85,18 +103,14 @@ class AccountRequestList(APIView):
         try:
             # Build serializer
             serializer = UserSerializer(data=request.data)
-
             # Validate request
             serializer.is_valid(raise_exception=True)
-
             # Save request
             account_request = serializer.save()
-
             # Account request serializer
             account_request_serializer = AccountRequestSerializer(
                 account_request
             )
-
             # Return the serialized user request
             return Response(
                 account_request_serializer.data, status=status.HTTP_201_CREATED
@@ -111,18 +125,18 @@ class AccountRequestList(APIView):
             )
 
 
+@extend_schema(
+    tags=["Account Request"],
+    description="Get an Account Request",
+)
 class AccountRequestDetail(APIView):
     """Get an Account Request"""
 
     def get_object(self, pk):
         """Get Account Request from db
-
         Args:
-
             pk: ObjectId
-
         Returns:
-
             Account Request
         """
         try:
@@ -130,23 +144,34 @@ class AccountRequestDetail(APIView):
         except exceptions.DoesNotExist:
             raise Http404
 
+    @extend_schema(
+        summary="Retrieve an Account Request",
+        description="Retrieve an Account Request",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="AccountRequest ID",
+            ),
+        ],
+        responses={
+            200: AccountRequestSerializer,
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     @method_decorator(api_staff_member_required())
     def get(self, request, pk):
-        """Retrieve a Account Request
-
+        """Retrieve an Account Request
         Parameters:
-
             {
-                "pk": "account_request_id"
+              "pk": "account_request_id"
             }
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 200
               content: Account Request
             - code: 404
@@ -157,10 +182,8 @@ class AccountRequestDetail(APIView):
         try:
             # Get object
             account_request_object = self.get_object(pk)
-
             # Serialize object
             serializer = AccountRequestSerializer(account_request_object)
-
             # Return response
             return Response(serializer.data)
         except Http404:
@@ -173,27 +196,51 @@ class AccountRequestDetail(APIView):
             )
 
 
+@extend_schema(
+    tags=["Account Request"],
+    description="Deny an Account Request",
+)
 class AccountRequestDeny(AbstractActionAccountRequest):
     """Deny an Account Request"""
 
+    @extend_schema(
+        summary="Deny an Account Request",
+        description="Deny an Account Request",
+        request=None,
+        responses={
+            200: OpenApiResponse(description="Account request denied"),
+            404: OpenApiResponse(description="Account request not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     def perform(self, account_request_object):
         """Deny an Account Request
-
         Args:
-
             account_request_object: account_request
         """
         account_request_api.deny(account_request_object)
 
 
+@extend_schema(
+    tags=["Account Request"],
+    description="Accept an Account Request",
+)
 class AccountRequestAccept(AbstractActionAccountRequest):
     """Accept an Account Request"""
 
+    @extend_schema(
+        summary="Accept an Account Request",
+        description="Accept an Account Request",
+        request=None,
+        responses={
+            200: OpenApiResponse(description="Account request accepted"),
+            404: OpenApiResponse(description="Account request not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     def perform(self, account_request_object):
         """Accept an Account Request
-
         Args:
-
             account_request_object: account_request
         """
         account_request_api.accept(account_request_object)
--- core_website_app/rest/contact_message/views.py
+++ core_website_app/rest/contact_message/views.py
@@ -5,6 +5,12 @@ import logging
 
 from django.http import Http404
 from django.utils.decorators import method_decorator
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    OpenApiResponse,
+    OpenApiParameter,
+    extend_schema,
+)
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -20,19 +26,27 @@ from core_website_app.rest.contact_message.serializers import (
 logger = logging.getLogger("core_website_app.rest.contact_message.views")
 
 
+@extend_schema(
+    tags=["Contact Message"],
+    description="Create or get all Contact Message",
+)
 class ContactMessageList(APIView):
     """Create or get all Contact Message"""
 
+    @extend_schema(
+        summary="Get all contact messages",
+        description="Get all contact messages",
+        responses={
+            200: ContactMessageSerializer(many=True),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     @method_decorator(api_staff_member_required())
     def get(self, request):
         """Get all Contact Message
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 200
               content: List of contact messages
             - code: 500
@@ -40,12 +54,10 @@ class ContactMessageList(APIView):
         """
         try:
             contact_message_list = contact_message_api.get_all()
-
             # Serialize object
             serializer = ContactMessageSerializer(
                 contact_message_list, many=True
             )
-
             # Return response
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as api_exception:
@@ -54,40 +66,41 @@ class ContactMessageList(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Create a new contact message",
+        description="Create a new contact message",
+        request=ContactMessageSerializer,
+        responses={
+            201: ContactMessageSerializer,
+            400: OpenApiResponse(
+                description="Validation error / missing parameters"
+            ),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     def post(self, request):
         """Create a message
-
         Parameters:
-
             {
-                "name": "name",
-                "content": "message",
-                "email": "email"
+              "name": "name",
+              "content": "message",
+              "email": "email"
             }
-
         Args:
-
             request: HTTP request
-
         Returns:
-
-            - code: 200
+            - code: 201
               content: Contact message
             - code: 400
               content: Validation error / missing parameters
-            - code: 500
-              content: Internal server error
         """
         try:
             # Build serializer
             serializer = ContactMessageSerializer(data=request.data)
-
             # Validate message
             serializer.is_valid(raise_exception=True)
-
             # Save message
             serializer.save()
-
             # Return the serialized message
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except ValidationError as validation_exception:
@@ -100,18 +113,18 @@ class ContactMessageList(APIView):
             )
 
 
+@extend_schema(
+    tags=["Contact Message"],
+    description="Get or delete a Contact Message",
+)
 class ContactMessageDetail(APIView):
     """Get or delete a Contact Message"""
 
     def get_object(self, pk):
         """Get Contact Message from db
-
         Args:
-
             pk: ObjectId
-
         Returns:
-
             Contact Message
         """
         try:
@@ -119,23 +132,34 @@ class ContactMessageDetail(APIView):
         except exceptions.DoesNotExist:
             raise Http404
 
+    @extend_schema(
+        summary="Retrieve a Contact Message",
+        description="Retrieve a Contact Message",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="ContactMessage ID",
+            ),
+        ],
+        responses={
+            200: ContactMessageSerializer,
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     @method_decorator(api_staff_member_required())
     def get(self, request, pk):
         """Retrieve a Contact Message
-
         Parameters:
-
             {
-                "pk": "message_id"
+              "pk": "message_id"
             }
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 200
               content: Contact message
             - code: 404
@@ -146,10 +170,8 @@ class ContactMessageDetail(APIView):
         try:
             # Get object
             contact_message_object = self.get_object(pk)
-
             # Serialize object
             serializer = ContactMessageSerializer(contact_message_object)
-
             # Return response
             return Response(serializer.data)
         except Http404:
@@ -161,23 +183,34 @@ class ContactMessageDetail(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Delete a Contact Message",
+        description="Delete a Contact Message",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="ContactMessage ID",
+            ),
+        ],
+        responses={
+            204: OpenApiResponse(description="Deletion succeed"),
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
     @method_decorator(api_staff_member_required())
     def delete(self, request, pk):
         """Delete a Contact Message
-
         Parameters:
-
             {
-                "pk": "message_id"
+              "pk": "message_id"
             }
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 204
               content: Deletion succeed
             - code: 404
@@ -188,10 +221,8 @@ class ContactMessageDetail(APIView):
         try:
             # Get object
             contact_message_object = self.get_object(pk)
-
             # delete object
             contact_message_api.delete(contact_message_object)
-
             # Return response
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Http404:
